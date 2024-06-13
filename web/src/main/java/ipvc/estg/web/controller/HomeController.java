7d40324d8088a66c96225082a5987923ed7b21ef
FranciscoSantos1 <francisco.santos@ipvc.pt>
2024-06-13 23:56:48 +0100

    responsavelInstrutores done.

--- web/src/main/java/ipvc/estg/web/controller/HomeController.java
+++ web/src/main/java/ipvc/estg/web/controller/HomeController.java
@@ -6,6 +6,8 @@ import bll.SocioBLL;
 import entity.Funcionario;
 import entity.Aula;
 import entity.Socio;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -84,4 +86,12 @@ public class HomeController {
         return "Aula eliminada com sucesso.";
     }
 
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+        return "redirect:/login";
+    }
 }
